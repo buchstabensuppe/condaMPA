eaa9b0062e593510571849ea99e80f6ae4e7d1a3
buchstabensuppe <143522877+buchstabensuppe@users.noreply.github.com>
2023-12-19 16:27:38 +0100

    MPI reactor can be called, but data file neets to be restructured, to fit sindys expectations. now its 4 tuples of 5000 units, but should be 5000 of 4 units

--- sindy/main_sindy.py
+++ sindy/main_sindy.py
@@ -2,72 +2,108 @@ import CSTR1
 from CSTR1 import simCSTR1
 import numpy as np
 import pysindy as ps
+from sklearn.metrics import mean_squared_error
 #CSTR Simulation aufrufen und Simulationsdaten importeren
-seconds =int(5) #[s]  <- WARNING if you change these numbers, also num at t=np.linspace has to be changed
-tstep = 0.001 #[s]
+import matplotlib.pyplot as plt
+
+
+seconds =5 #[s]  <- WARNING if you change these numbers, also num at t=np.linspace has to be changed
+dt = 0.001 #[s]
 n_variables = 3
-x0s = [100, 50, 0]
-data = simCSTR1(seconds, tstep, n_variables, x0s)
+x0s = [100, 50, 10]
+data = simCSTR1(seconds, dt, n_variables, x0s)
 
-############ sindy ################
-t = np.linspace(0, 1, 5000)
+# mein CSTR in original namen übersetzen
 
-feature_names=["a", "b", "c"]
-#
-# opt = pysindy.STLSQ(threshold=0.004)
-# sindy_opt = ps.SINDyPI(
-#     threshold=1e-6,
-#     tol=1e-8,
-#     thresholder="l1",
-#     max_iter=20000,
-#)
-#
-# sindy_library = ps.SINDyPILibrary(
-#     library_functions=library_functions,
-#     x_dot_library_functions=x_dot_library_functions,
-#     t=t,
-#     function_names=library_function_names,
-#     include_bias=True,
-# )
-# opt = ps.SR3(
-#     threshold=0.04, thresholder="l1", max_iter=1000000, normalize_columns=True, tol=1e-6
-# )
-#opt = ps.SINDy()
-#opt = ps.BaseOptimizer(0)
-model = ps.SINDy(feature_names=feature_names, optimizer=opt)
+t_train = np.arange(0, seconds, dt)
+t_train_span = (t_train[0], t_train[-1])
+u_train = data
+u_train_clean = u_train
+# Instantiate and fit the SINDy model with u_dot
+u_dot = ps.FiniteDifference()._differentiate(u_train, t=dt)
+library_functions = [lambda x: x, lambda x: x * x, lambda x, y: x * y]
+library_function_names = [lambda x: x, lambda x: x + x, lambda x, y: x + y]
 
+# Instantiate and fit a non-weak SINDy model
+ode_lib = ps.CustomLibrary(
+    library_functions=library_functions,
+    function_names=library_function_names,
+    include_bias=True,
+)
+optimizer = ps.SR3(
+    threshold=100, thresholder="l0", max_iter=10000, normalize_columns=True, tol=1e-10
+)
+original_model = ps.SINDy(feature_library=ode_lib, optimizer=optimizer)
+original_model.fit(u_train, t=dt, quiet=True)
+print("Regular model: ")
+original_model.print()
+err_not_weak = np.sqrt(
+    (np.sum((u_dot - optimizer.Theta_ @ optimizer.coef_.T) ** 2) / np.sum(u_dot**2))
+    / u_dot.shape[0]
+)
 
-model.fit(data, t=t)
-model.print()
-############## calling sindy (out of different .py) with multiple trajectories ##########
-#
-# ssr_optimizer = ps.SSR()
-# model = ps.SINDy(optimizer=ssr_optimizer)
-
-
-#
-# ############ sindy threshold scan ################
-# threshold_scan = np.linspace(0,1,11)
-# coefs = []
-#
-# rmse = mean_square_error(x_train,
-#                          np.zeros(x_train.shape,),
-#                          squared = False)
-#
-# for i, threshold in enumerate(threshold_scan):
-#     opt = ps.STLSQ(threshold=threshold)
-#     model = ps.SINDy(feature_names=feature_names, optimizer=opt)
-#     model.fit()
+u_pred = original_model.simulate(x0s, t=t_train, integrator="odeint")
+# um u_dot zu predicten muss das Anfangswertproblem für das original_model gelöst werden. Eventuell mit solve_ivp von https://pysindy.readthedocs.io/en/latest/examples/12_weakform_SINDy_examples/example.html
+# u_dot_pred = original_model.predict(u_test)
+print(u_pred)
 
 
-## sindy example ##
-#
-# t = np.linspace(0, 1, 100)
-# x = 3 * np.exp(-2 * t)
-# y = 0.5 * np.exp(t)
-# X = np.stack((x, y), axis=-1)  # First column is x, second is y
-# model = ps.SINDy(feature_names=["x", "y"])
-# model.fit(X, t=t)
-# model.print()
+#jetzt einmal mit noise, dann nochmal ausgeben und über erstes Ergebnis plotten
 
-simtest(seconds, tstep, n_variables)
+#### noise auf die Daten packen:
+u_test = np.copy(u_train)
+rmse = mean_squared_error(u_train, np.zeros((u_train).shape), squared=False)
+u_dot_clean = ps.FiniteDifference()._differentiate(u_test, t=dt)
+u_clean = u_test
+u_train = u_train + np.random.normal(0, rmse / 20.0, u_train.shape)  # Add 20% noise
+print('u_train with noise:', u_train)
+rmse = mean_squared_error(u_test, np.zeros(u_test.shape), squared=False)
+u_test = u_test + np.random.normal(0, rmse / 20.0, u_test.shape)  # Add 20% noise
+u_dot = ps.FiniteDifference()._differentiate(u_test, t=dt)
+
+
+# Instantiate and fit the SINDy model with u_dot
+u_dot = ps.FiniteDifference()._differentiate(u_train, t=dt)
+library_functions = [lambda x: x, lambda x: x * x, lambda x, y: x * y]
+library_function_names = [lambda x: x, lambda x: x + x, lambda x, y: x + y]
+
+# Instantiate and fit a non-weak SINDy model
+ode_lib = ps.CustomLibrary(
+    library_functions=library_functions,
+    function_names=library_function_names,
+    include_bias=True,
+)
+optimizer = ps.SR3(
+    threshold=100, thresholder="l0", max_iter=10000, normalize_columns=True, tol=1e-10
+)
+model_verrauscht = ps.SINDy(feature_library=ode_lib, optimizer=optimizer)
+model_verrauscht.fit(u_train, t=dt, quiet=True)
+print("Regular model: ")
+model_verrauscht.print()
+err_not_weak = np.sqrt(
+    (np.sum((u_dot - optimizer.Theta_ @ optimizer.coef_.T) ** 2) / np.sum(u_dot**2))
+    / u_dot.shape[0]
+)
+
+u_pred_rauschen = model_verrauscht.simulate(x0s, t=t_train, integrator="odeint")
+print(u_pred_rauschen)
+# Plot the ground truth, weak form, and non-weak trajectory predictions
+feature_names = ["x", "y", "z"]
+u_weak = original_model.simulate(x0s, t=t_train, integrator="odeint")
+
+
+plt.figure(figsize=(16, 4))
+for i in range(3):
+    plt.subplot(1, 3, i + 1)
+    plt.plot(t_train, u_test[:, i], "c", label=r"$q$ with added noise")
+    plt.plot(t_train, u_pred[:, i], "r", label=r"$q$ prediction")
+    plt.plot(t_train, u_pred_rauschen[:, i], "b", label=r"$q$ weak form prediction")
+    plt.plot(t_train, u_train_clean[:, i], "k", label=r"$q$ without added noise")
+    plt.grid(True)
+    plt.ylabel(feature_names[i], fontsize=14)
+    plt.xlabel("t", fontsize=14)
+    plt.ylim(-100, 100)
+    if i == 2:
+        plt.legend()
+
+plt.show()
