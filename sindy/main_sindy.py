be2c98cc06f5a2b867a8331c56926c08aa7dc025
buchstabensuppe <143522877+buchstabensuppe@users.noreply.github.com>
2024-01-30 20:18:31 +0100

    normal sindy working with both reactors now, good results

--- sindy/main_sindy.py
+++ sindy/main_sindy.py
@@ -5,13 +5,42 @@ import pysindy as ps
 from sklearn.metrics import mean_squared_error
 #CSTR Simulation aufrufen und Simulationsdaten importeren
 import matplotlib.pyplot as plt
+from MPIR_callable_function import MPI_reactor
 
 
 seconds =5 #[s]  <- WARNING if you change these numbers, also num at t=np.linspace has to be changed
 dt = 0.001 #[s]
 n_variables = 3
-x0s = [100, 50, 10]
-data = simCSTR1(seconds, dt, n_variables, x0s)
+seconds = 1 #[s]  <- WARNING if you change these numbers, also num at t=np.linspace has to be changed
+dt = 0.0002 #[s]
+dt_time_seconds = int(seconds/dt)
+
+# Simulation auswählen: basic batch reactor = 1, MPI CSTR = 2
+reactor_choice = 2
+if reactor_choice == 1:
+    n_variables = 3
+    x0s = [100, 50, 10]
+    data = simCSTR1(seconds, dt, n_variables, x0s)
+
+if reactor_choice == 2:
+    n_variables = 4
+    x0 = np.array([0.8, 0.2, 0, 0])
+    x0_test = np.array([0.75, 0.3, 0, 0])
+    data_raw = MPI_reactor(seconds, dt_time_seconds, x0)
+    data_raw_test = MPI_reactor(seconds, dt_time_seconds, x0_test)
+    x0s = data_raw[0]
+    x0s_test = data_raw_test[0]
+    data_tmp = np.array(data_raw)
+    data_tmp_test = np.array(data_raw_test)
+    data = np.zeros((5000, 4))
+    data_test = np.zeros((5000,4))
+    for i in range(dt_time_seconds):
+        data[i] = [data_tmp[0, i], data_tmp[1, i], data_tmp[2, i], data_tmp[3, i]]
+        data_test[i] = [data_tmp_test[0, i], data_tmp_test[1, i], data_tmp_test[2, i], data_tmp_test[3, i]]
+    x0s = data[0]
+    x0s_test = data_test[0]
+
+
 
 # mein CSTR in original namen übersetzen
 
@@ -55,10 +84,10 @@ u_test = np.copy(u_train)
 rmse = mean_squared_error(u_train, np.zeros((u_train).shape), squared=False)
 u_dot_clean = ps.FiniteDifference()._differentiate(u_test, t=dt)
 u_clean = u_test
-u_train = u_train + np.random.normal(0, rmse / 80.0, u_train.shape)  # Add 20% noise
+u_train = u_train + np.random.normal(0, rmse / 160.0, u_train.shape)  # Add 20% noise
 print('u_train with noise:', u_train)
 rmse = mean_squared_error(u_test, np.zeros(u_test.shape), squared=False)
-u_test = u_test + np.random.normal(0, rmse / 80.0, u_test.shape)  # Add 20% noise
+u_test = u_test + np.random.normal(0, rmse / 160.0, u_test.shape)  # Add 20% noise
 u_dot = ps.FiniteDifference()._differentiate(u_test, t=dt)
 
 
